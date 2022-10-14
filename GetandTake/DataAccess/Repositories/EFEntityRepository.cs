57126ba79e643a79e1777b02dfba3a4c6b14775c
Ceren Susuz <ceren199704@hotmail.com>
2022-10-14 14:28:45 +0300

    created repositories for CRUD db processes

---
+++ GetandTake/DataAccess/Repositories/EFEntityRepository.cs
@@ -0,0 +1,62 @@
+﻿using GetandTake.DataAccessLayer.EF;
+using GetandTake.Models.BaseModels;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace GetandTake.DataAccess.Repositories
+{
+    public class EFEntityRepository<TEntity> : IEntityRepository<TEntity>
+        where TEntity : class, IBaseEntity, new()
+    {
+        private readonly NorthwindDbContext _context;
+        private readonly DbSet<TEntity> _entities;
+        public EFEntityRepository(NorthwindDbContext context)
+        {
+            _context = context;
+            _entities = _context.Set<TEntity>();
+        }
+        public IQueryable<TEntity> Table => _entities;
+
+        public IQueryable<TEntity> AsNoTracking => _entities.AsNoTracking();
+
+        public void Add(TEntity entity)
+        {
+            var addedEntity = _context.Entry(entity);
+            addedEntity.State = EntityState.Added;
+            _context.SaveChanges();
+        }
+
+        public void Delete(TEntity entity)
+        {
+            var deletedEntity = _context.Entry(entity);
+            deletedEntity.State = EntityState.Deleted;
+            _context.SaveChanges();
+        }
+
+        public TEntity Get(Expression<Func<TEntity, bool>> filter)
+        {
+            var entity = _entities.FirstOrDefault(filter);
+            if (entity != null)
+                _context.Entry(entity).State = EntityState.Detached;
+            return entity;
+        }
+
+        public List<TEntity> GetAll(Expression<Func<TEntity, bool>>? filter = null)
+        {
+            return filter == null
+                                 ? _entities.ToList()
+                                 : _entities.Where(filter).ToList();
+        }
+
+        public void Update(TEntity entity)
+        {
+            var oldEntity = Get(entity => entity.Id == entity.Id);
+            if (oldEntity == null)
+                throw new(entity.Id + " id, is not found for " + typeof(TEntity).Name);
+            entity.CreatedAt = oldEntity.CreatedAt;
+            entity.UpdatedAt = DateTime.Now;
+            _context.Update(entity);
+            _context.SaveChanges();
+        }
+    }
+}
---
+++ GetandTake/DataAccess/Repositories/IEntityRepository.cs
@@ -0,0 +1,21 @@
+﻿using GetandTake.Models.BaseModels;
+using System.Linq.Expressions;
+using System.Security.Principal;
+
+namespace GetandTake.DataAccess.Repositories
+{
+    public interface IEntityRepository<TEntity>
+            where TEntity : class, IBaseEntity, new()
+    {
+        IQueryable<TEntity> Table { get; }
+        /// <summary>
+        /// To Table as No Tracking
+        /// </summary>
+        IQueryable<TEntity> AsNoTracking { get; }
+        List<TEntity> GetAll(Expression<Func<TEntity, bool>>? filter = null);
+        TEntity Get(Expression<Func<TEntity, bool>> filter);
+        void Add(TEntity entity);
+        void Update(TEntity entity);
+        void Delete(TEntity entity);
+    }
+}
