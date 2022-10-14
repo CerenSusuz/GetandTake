0d6924480ec427a1cecf8970db75bdc103b6f960
Ceren Susuz <ceren199704@hotmail.com>
2022-10-14 13:09:35 +0300

    edit some details

---
+++ GetandTake/Models/DTOs/ProductDTO.cs
@@ -0,0 +1,17 @@
+﻿namespace GetandTake.Models.DTOs
+{
+    public class ProductDTO
+    {
+        public int ProductID { get; set; }
+        public string ProductName { get; set; }
+        public string? QuantityPerUnit { get; set; }
+        public int? UnitPrice { get; set; }
+        public int? UnitsInStock { get; set; }
+        public int? UnitsOnOrder { get; set; }
+        public int? ReorderLevel { get; set; }
+        public bool? Discontinued { get; set; }
+
+        public string Category { get; set; }
+        public string Supplier { get; set; }
+    }
+}
--- GetandTake/Models/Product.cs
+++ GetandTake/Models/Product.cs
@@ -6,15 +6,15 @@
         public string ProductName { get; set; }
         public string? QuantityPerUnit { get; set; }
         public int? UnitPrice { get; set; }
-        public int? UnitsInStock { get; set; }
-        public int? UnitsOnOrder { get; set; }
-        public int? ReorderLevel { get; set; }
+        public short? UnitsInStock { get; set; }
+        public decimal? UnitsOnOrder { get; set; }
+        public short? ReorderLevel { get; set; }
         public bool? Discontinued { get; set; }
 
         public int? SupplierID { get; set; }
-        public Supplier SupplierName { get; set; }
+        public Supplier Supplier { get; set; }
         public int? CategoryID { get; set; }
-        public Category CategoryName { get; set; }
+        public Category Category { get; set; }
 
     }
 }
