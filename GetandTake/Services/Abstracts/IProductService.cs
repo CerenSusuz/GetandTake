7c61bca709a3c13d31ac9b6d7f10dc9dcd7f5985
Ceren Susuz <ceren199704@hotmail.com>
2022-10-14 15:42:18 +0300

    created services for product

---
+++ GetandTake/Services/Abstracts/IProductService.cs
@@ -0,0 +1,17 @@
+﻿using GetandTake.Models.Base;
+using GetandTake.Models.DTOs.BaseDTO;
+using GetandTake.Models.DTOs.ListDTO;
+
+namespace GetandTake.Services.Abstracts
+{
+    public interface IProductService
+    {
+        Task<List<ProductsDTO>> GetAllAsync();
+        Task<List<ProductsDTO>> GetAllByCategoryAsync(int categoryId);
+        Task<List<ProductsDTO>> GetAllBySupplierAsync(int supplierId);
+        Task<ProductsDTO> GetAsync(int id);
+        Task UpdateAsync(int id, ProductDTO dto);
+        Task InsertAsync(ProductDTO dto);
+        Task DeleteAsync(int id);
+    }
+}
---
+++ GetandTake/Services/Concretes/ProductManager.cs
@@ -0,0 +1,75 @@
+﻿using AutoMapper;
+using GetandTake.DataAccess.Repositories;
+using GetandTake.Models;
+using GetandTake.Models.DTOs.BaseDTO;
+using GetandTake.Models.DTOs.ListDTO;
+using GetandTake.Services.Abstracts;
+using Microsoft.EntityFrameworkCore;
+
+namespace GetandTake.Services.Concretes
+{
+    public class ProductManager : IProductService
+    {
+        private readonly IEntityRepository<Product> _repository;
+        private readonly IMapper _mapper;
+        public ProductManager(IEntityRepository<Product> repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+        public async Task DeleteAsync(int id)
+        {
+            var findProduct = await _repository.GetAsync(entity => entity.ProductID == id);
+            await _repository.DeleteAsync(findProduct);
+        }
+
+        public async Task<List<ProductsDTO>> GetAllAsync()
+        {
+            var products = _repository.AsNoTracking
+                .Include(p => p.Category);
+            return _mapper.Map<List<ProductsDTO>>(products);
+        }
+
+        public async Task<List<ProductsDTO>> GetAllByCategoryAsync(int categoryId)
+        {
+            var products = _repository.AsNoTracking
+                .Where(c => c.CategoryID == categoryId)
+                .Include(p => p.Category);
+            return _mapper.Map<List<ProductsDTO>>(products);
+        }
+
+        public async Task<List<ProductsDTO>> GetAllBySupplierAsync(int supplierId)
+        {
+            var products = _repository.AsNoTracking
+                .Where(c => c.SupplierID == supplierId)
+                .Include(p => p.Supplier);
+            return _mapper.Map<List<ProductsDTO>>(products);
+        }
+
+        public async Task<ProductsDTO> GetAsync(int id)
+        {
+            var findProduct = _repository.AsNoTracking
+                .Include(p => p.Category)
+                .Where(p => p.ProductID == id)
+                .FirstOrDefault();
+            return _mapper.Map<ProductsDTO>(findProduct);
+        }
+
+        public async Task InsertAsync(ProductDTO dto)
+        {
+            var product = _mapper.Map<Product>(dto);
+            await _repository.InsertAsync(product);
+        }
+
+        public async Task UpdateAsync(int id, ProductDTO dto)
+        {
+            var findProduct = await _repository.GetAsync(entity => entity.ProductID == id);
+            if (findProduct != null)
+            {
+                var product = _mapper.Map<Product>(dto);
+                product.ProductID = id;
+                await _repository.UpdateAsync(product);
+            }
+        }
+    }
+}
