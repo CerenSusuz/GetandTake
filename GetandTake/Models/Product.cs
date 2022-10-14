145708221f438a378c837d4fa91024b8278b7193
Ceren Susuz <ceren199704@hotmail.com>
2022-10-14 13:00:37 +0300

    created some models

---
+++ GetandTake/Models/Product.cs
@@ -0,0 +1,20 @@
+﻿namespace GetandTake.Models
+{
+    public class Product
+    {
+        public int ProductID { get; set; }
+        public string ProductName { get; set; }
+        public string? QuantityPerUnit { get; set; }
+        public int? UnitPrice { get; set; }
+        public int? UnitsInStock { get; set; }
+        public int? UnitsOnOrder { get; set; }
+        public int? ReorderLevel { get; set; }
+        public bool? Discontinued { get; set; }
+
+        public int? SupplierID { get; set; }
+        public Supplier SupplierName { get; set; }
+        public int? CategoryID { get; set; }
+        public Category CategoryName { get; set; }
+
+    }
+}
