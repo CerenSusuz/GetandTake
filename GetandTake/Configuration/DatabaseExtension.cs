782f9f0ac6e2545ed8188164a52c018f29ce3fb4
CerenSusuz <ceren_susuz@epam.com>
2022-10-17 21:32:38 +0300

    edit db extensions and add services IoC

---
+++ GetandTake/Configuration/DatabaseExtension.cs
@@ -0,0 +1,16 @@
+﻿using GetandTake.DataAccess;
+using Microsoft.EntityFrameworkCore;
+
+namespace GetandTake.Configuration;
+
+public static class DatabaseExtension
+{
+    public static void RegisterDatabase(WebApplicationBuilder builder)
+    {
+        builder.Services.AddDbContext<NorthwindDbContext>(options =>
+            {
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+            }
+        );
+    }
+}
---
+++ GetandTake/Configuration/ServiceExtensions.cs
@@ -0,0 +1,17 @@
+﻿using GetandTake.DataAccess.Repositories.Abstract;
+using GetandTake.DataAccess.Repositories.Concrete;
+using GetandTake.Services.Abstract;
+using GetandTake.Services.Concrete;
+
+namespace GetandTake.Configuration;
+
+public static class ServiceExtensions
+{
+    public static void RegisterServices(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddScoped<IProductService, ProductManager>();
+        builder.Services.AddScoped<IProductRepository, ProductRepository>();
+        builder.Services.AddScoped<ICategoryService, CategoryManager>();
+        builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
+    }
+}
