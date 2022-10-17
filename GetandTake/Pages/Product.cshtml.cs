f60b6767efdede4c3213af1050f7cac034d21411
CerenSusuz <ceren_susuz@epam.com>
2022-10-17 21:40:50 +0300

    changed from getall to getbymaximumamount

--- GetandTake/Pages/Product.cshtml.cs
+++ GetandTake/Pages/Product.cshtml.cs
@@ -1,22 +1,30 @@
+using GetandTake.Configuration;
 using GetandTake.Models.DTOs.ListDTO;
-using GetandTake.Services.Abstracts;
+using GetandTake.Services.Abstract;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace GetandTake.Pages;
 
 public class ProductModel : PageModel
 {
+
     private readonly IProductService _productService;
+
+    private readonly IConfiguration Configuration;
 
     public IEnumerable<ProductsDTO> Products { get; private set; }
 
-    public ProductModel(IProductService productService)
+    public int AmountOfProduct { get; private set; }
+
+    public ProductModel(IProductService productService, IConfiguration configuration)
     {
         _productService = productService;
+        Configuration = configuration;
     }
 
     public void OnGet()
     {
-        Products = _productService.GetAll();
+        AmountOfProduct = Convert.ToInt32(Configuration.GetSection("MaximumAmountOfProduct").Value);
+        Products = _productService.GetByMaximumAmount(AmountOfProduct);
     }
 }
