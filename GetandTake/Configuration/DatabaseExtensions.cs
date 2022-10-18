11f6385bd6dd6066276f5c553e29f639257d6ddd
CerenSusuz <ceren_susuz@epam.com>
2022-10-18 14:03:52 +0300

    editted

---
+++ GetandTake/Configuration/DatabaseExtensions.cs
@@ -0,0 +1,19 @@
+﻿using GetandTake.DataAccess;
+using Microsoft.EntityFrameworkCore;
+
+namespace GetandTake.Configuration;
+
+public static class DatabaseExtensions
+{
+    public static void RegisterDatabase(
+        this WebApplicationBuilder builder,
+        DatabaseSettings databaseSettings
+        )
+    {
+        builder.Services.AddDbContext<NorthwindDbContext>(options =>
+        {
+            options.UseSqlServer(databaseSettings.DefaultConnection);
+        }
+        );
+    }
+}
