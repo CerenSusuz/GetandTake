c338afad43d23bdf8ff137bada05bf0ff5b87aa3
CerenSusuz <ceren_susuz@epam.com>
2022-10-18 15:39:20 +0300

    move under Products folder

---
+++ GetandTake/Pages/Products/Product.cshtml.cs
@@ -0,0 +1,34 @@
+using GetandTake.Configuration;
+using GetandTake.Models.DTOs.ListDTO;
+using GetandTake.Services.Abstract;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace GetandTake.Pages.Products;
+
+public class ProductModel : PageModel
+{
+
+    private readonly IProductService _productService;
+
+    private readonly AppSettings _appSettings;
+
+    public IEnumerable<ProductsDTO> Products { get; private set; }
+
+    public int AmountOfProduct { get; private set; }
+
+    public ProductModel
+        (
+        IProductService productService,
+        AppSettings appSettings
+        )
+    {
+        _productService = productService;
+        _appSettings = appSettings;
+    }
+
+    public void OnGet()
+    {
+        AmountOfProduct = _appSettings.Products.MaximumAmount;
+        Products = _productService.GetByMaximumAmount(AmountOfProduct);
+    }
+}
