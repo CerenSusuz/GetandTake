28d177c73ea577436eb088e5490c79027c6d2a65
CerenSusuz <ceren_susuz@epam.com>
2022-10-18 16:40:13 +0300

    created supplier service and manager

---
+++ GetandTake/Services/Abstract/ISupplierService.cs
@@ -0,0 +1,16 @@
+﻿using GetandTake.Models;
+
+namespace GetandTake.Services.Abstract;
+
+public interface ISupplierService
+{
+    IEnumerable<Supplier> GetAll();
+
+    Supplier GetById(int supplierId);
+
+    Task UpdateAsync(int supplierId, Supplier supplier);
+
+    Task CreateAsync(Supplier supplier);
+
+    void Delete(int supplierId);
+}
---
+++ GetandTake/Services/Concrete/SupplierManager.cs
@@ -0,0 +1,50 @@
+﻿using GetandTake.DataAccess.Repositories.Abstract;
+using GetandTake.Models;
+using GetandTake.Services.Abstract;
+
+namespace GetandTake.Services.Concrete;
+
+public class SupplierManager : ISupplierService
+{
+    private readonly ISupplierRepository _repository;
+
+    public SupplierManager(ISupplierRepository repository)
+    {
+        _repository = repository;
+    }
+
+    public async Task CreateAsync(Supplier supplier)
+    {
+        await _repository.CreateAsync(supplier);
+    }
+
+    public void Delete(int supplierId)
+    {
+        _repository.Delete(entity => entity.SupplierID == supplierId);
+    }
+
+    public IEnumerable<Supplier> GetAll()
+    {
+        var suppliers = _repository.GetAll();
+
+        return suppliers;
+    }
+
+    public Supplier GetById(int supplierId)
+    {
+        var findSupplier = _repository.AsNoTracking()
+            .First(supplier => supplier.SupplierID == supplierId);
+
+        return findSupplier;
+    }
+
+    public async Task UpdateAsync(int supplierId, Supplier supplier)
+    {
+        var findSupplier = await _repository.GetAsync(category => category.SupplierID == supplierId);
+        if (findSupplier != null)
+        {
+            supplier.SupplierID = supplierId;
+            _repository.Update(supplier);
+        }
+    }
+}
