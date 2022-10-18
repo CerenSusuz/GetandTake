95a2ab9bd970f7d8755f01af02417205cf8853f7
CerenSusuz <ceren_susuz@epam.com>
2022-10-18 18:17:40 +0300

    add supplier

--- GetandTake/Pages/Products/Create.cshtml.cs
+++ GetandTake/Pages/Products/Create.cshtml.cs
@@ -1,38 +1,49 @@
-using GetandTake.Configuration;
 using GetandTake.Models;
 using GetandTake.Models.DTOs.BaseDTO;
-using GetandTake.Models.DTOs.ListDTO;
 using GetandTake.Services.Abstract;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace GetandTake.Pages.Products
+namespace GetandTake.Pages.Products;
+
+[BindProperties]
+public class CreateModel : PageModel
 {
-    public class CreateModel : PageModel
+
+    private readonly IProductService _productService;
+
+    private readonly ICategoryService _categoryService;
+
+    private readonly ISupplierService _supplierService;
+
+    public IEnumerable<Category> Categories { get; private set; }
+
+    public IEnumerable<Supplier> Suppliers { get; private set; }
+
+    public ProductDTO ProductDTO { get; set; }
+
+    public CreateModel
+        (
+        IProductService productService,
+        ICategoryService categoryService,
+        ISupplierService supplierService
+        )
     {
+        _productService = productService;
+        _categoryService = categoryService;
+        _supplierService = supplierService;
+    }
 
-        private readonly IProductService _productService;
+    public void OnGet()
+    {
+        Categories = _categoryService.GetAll();
+        Suppliers = _supplierService.GetAll();
+    }
 
-        private readonly ICategoryService _categoryService;
-
-        public IEnumerable<Category> Categories { get; private set; }
-        public ProductDTO ProductDTO { get; private set; }
-
-        public CreateModel(IProductService productService, ICategoryService categoryService)
-        {
-            _productService = productService;
-            _categoryService = categoryService;
-        }
-
-        public void OnGet()
-        {
-            Categories = _categoryService.GetAll();
-        }
-
-        public async Task<IActionResult> OnPost(ProductDTO productDTO)
-        {
-            await _productService.CreateAsync(productDTO);
-            return RedirectToPage("Product");
-        }
+    public async Task<IActionResult> OnPost(ProductDTO productDTO)
+    {
+        await _productService.CreateAsync(productDTO);
+        return RedirectToPage("Product");
     }
 }
+
