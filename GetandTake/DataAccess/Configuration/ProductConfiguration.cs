30d91ec68b2ef775551f4f272dfebe267ab193cd
CerenSusuz <ceren_susuz@epam.com>
2022-10-20 12:45:36 +0300

    added entity configuration

---
+++ GetandTake/DataAccess/Configuration/ProductConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using GetandTake.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace GetandTake.DataAccess.Configuration;
+
+public class ProductConfiguration : IEntityTypeConfiguration<Product>
+{
+    public void Configure(EntityTypeBuilder<Product> builder)
+    {
+        builder.ToTable("Products");
+        builder.Property(property => property.ProductName).IsRequired();
+        builder
+            .HasOne<Category>()
+            .WithMany(category => category.Products)
+            .HasForeignKey(product => product.CategoryID)
+            .OnDelete(DeleteBehavior.SetNull)
+            .IsRequired(required: false);
+
+        builder
+            .HasOne<Supplier>()
+            .WithMany(supplier => supplier.Products)
+            .HasForeignKey(product => product.SupplierID)
+            .OnDelete(DeleteBehavior.SetNull)
+            .IsRequired(required: false);
+    }
+}
---
+++ GetandTake/DataAccess/Configuration/SupplierConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using GetandTake.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace GetandTake.DataAccess.Configuration;
+
+public class SupplierConfiguration : IEntityTypeConfiguration<Supplier>
+{
+    public void Configure(EntityTypeBuilder<Supplier> builder)
+    {
+        builder.ToTable("Suppliers");
+        builder.Property(prop => prop.CompanyName).IsRequired();
+    }
+}
