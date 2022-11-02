c9938a9823a1e0a2f4bd83f697ae4e2b9b392a14
CerenSusuz <ceren_susuz@epam.com>
2022-11-02 11:43:06 +0300

    create controller for unit test

---
+++ GetandTake/Controller/ProductController.cs
@@ -0,0 +1,29 @@
+﻿using GetandTake.Models.DTOs.ListDTO;
+using GetandTake.Services.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GetandTake.Controller;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ProductController : ControllerBase
+{
+    private readonly IProductService productService;
+    public ProductController(IProductService _productService)
+    {
+        productService = _productService;
+    }
+
+    [HttpGet("productlist")]
+    public async Task<List<ProductsDTO>> ProductList()
+    {
+        var productList = await productService.GetAllAsync();
+        return productList;
+
+    }
+    [HttpGet("getproductbyid")]
+    public async Task<ProductsDTO> GetProductById(int id)
+    {
+        return await productService.GetByIdAsync(id);
+    }
+}
