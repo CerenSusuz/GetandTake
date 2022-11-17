fd3e0c591270a8176305f65cb84be5f203cfc79e
CerenSusuz <ceren_susuz@epam.com>
2022-11-17 18:28:16 +0300

    Create a View Component to visualize simple breadcrumbs.

---
+++ GetandTake/ViewComponents/CategoryViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using GetandTake.Services.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GetandTake.ViewComponents;
+
+public class CategoryViewComponent : ViewComponent
+{
+    private readonly ICategoryService _categoryService;
+
+    public CategoryViewComponent(ICategoryService categoryService)
+    {
+        _categoryService = categoryService;
+    }
+
+    public async Task<IViewComponentResult> InvokeAsync()
+    {
+        var categories = await _categoryService.GetAllAsync();
+
+        return View(categories);
+    }
+}
