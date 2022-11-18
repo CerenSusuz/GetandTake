722e1b37fca93a8fc26e00d25fde91ef46698d6e
CerenSusuz <ceren_susuz@epam.com>
2022-11-18 16:31:51 +0300

    removed string message

--- GetandTake/Core/Filters/LogActionFilterAttribute.cs
+++ GetandTake/Core/Filters/LogActionFilterAttribute.cs
@@ -13,21 +13,21 @@ public class LogActionFilterAttribute : ActionFilterAttribute
 
     public override void OnActionExecuting(ActionExecutingContext filterContext)
     {
-        _logger.LogInformation($"Log action: {0} {1}",nameof(OnActionExecuting), filterContext.RouteData.Values["page"]);
+        _logger.LogInformation(nameof(OnActionExecuting), filterContext.RouteData.Values["page"]);
     }
 
     public override void OnActionExecuted(ActionExecutedContext filterContext)
     {
-        _logger.LogInformation($"Log action: {nameof(OnActionExecuted)}{filterContext.RouteData.Values["page"]}");
+        _logger.LogInformation(nameof(OnActionExecuted), filterContext.RouteData.Values["page"]);
     }
 
     public override void OnResultExecuting(ResultExecutingContext filterContext)
     {
-        _logger.LogInformation($"Log action: {nameof(OnResultExecuting)}{filterContext.RouteData.Values["page"]}");
+        _logger.LogInformation(nameof(OnResultExecuting), filterContext.RouteData.Values["page"]);
     }
 
     public override void OnResultExecuted(ResultExecutedContext filterContext)
     {
-        _logger.LogInformation($"Log action: {nameof(OnResultExecuted)}{filterContext.RouteData.Values["page"]}");
+        _logger.LogInformation(nameof(OnResultExecuted), filterContext.RouteData.Values["page"]);
     }
 }
