8471dc2ad088588f1606df7922d04b088b8d9e63
CerenSusuz <ceren_susuz@epam.com>
2022-11-22 10:28:12 +0300

    created html-tag helpers

---
+++ GetandTake/Helpers/ImageLinkHtmlHelperExtension.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Mvc;
+
+namespace GetandTake.Helpers;
+
+public static class ImageLinkHtmlHelperExtension
+{
+    public static MvcHtmlString ProduceImageLink(int categoryImageId, string linkText)
+    {
+        var anchorBuilder = new TagBuilder("a");
+        anchorBuilder.MergeAttribute("src", "@imagePath");
+        anchorBuilder.MergeAttribute("class", "btn btn-outline-success");
+        anchorBuilder.MergeAttribute("asp-page", "Image");
+        anchorBuilder.MergeAttribute("asp-route-id", $"@category.{categoryImageId}");
+
+        return new MvcHtmlString(anchorBuilder.ToString());
+    }
+}
---
+++ GetandTake/Helpers/ImageLinkTagHelper.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc.TagHelpers;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System.Text.Encodings.Web;
+
+namespace GetandTake.Helpers;
+
+[HtmlTargetElement("getandtake-image-link", Attributes = CustomAttributeName)]
+public class ImageLinkTagHelper : TagHelper
+{
+    private const string CustomAttributeName = "category-image-id";
+
+    [HtmlAttributeName(CustomAttributeName)]
+    public string CategoryID { get; set; }
+
+    public override void Process(TagHelperContext context, TagHelperOutput output)
+    {
+        output.TagName = "a";
+        output.TagMode = TagMode.StartTagAndEndTag;
+        output.Attributes.Add("asp-page", "Image");
+        output.AddClass("btn btn-outline-success", HtmlEncoder.Default);
+        output.Content.SetContent("Show Image");
+        output.Attributes.Add("asp-route-id", $"@category.{CategoryID}");
+    }
+}
