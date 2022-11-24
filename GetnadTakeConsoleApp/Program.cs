860ae88e0f126434956def4850e8fca328188f81
CerenSusuz <ceren_susuz@epam.com>
2022-11-24 11:56:47 +0300

    created consoleapp

---
+++ GetnadTakeConsoleApp/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Net.Http.Headers;
+
+static void Main()
+{
+    RunAsync().GetAwaiter().GetResult();
+}
+
+static async Task RunAsync()
+{
+
+    HttpClient client = new()
+    {
+        BaseAddress = new Uri("https://localhost:7167/api")
+    };
+    client.DefaultRequestHeaders.Accept.Clear();
+    client.DefaultRequestHeaders.Accept.Add(
+        new MediaTypeWithQualityHeaderValue("application/json"));
+
+    try
+    {
+        var products = await client.GetAsync(client.BaseAddress + "/products");
+        var categories = await client.GetAsync(client.BaseAddress + "/categories");
+
+        if (products.IsSuccessStatusCode && categories.IsSuccessStatusCode)
+        {
+            Console.WriteLine($"Products : {products} / Categories {categories}");
+        }      
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine(e.Message);
+    }
+
+    Console.ReadLine();
+}
