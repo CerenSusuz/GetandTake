fe1c2d7520ecc7ceff130f46fa1bbbc88213bf2e
CerenSusuz <ceren_susuz@epam.com>
2022-11-24 11:21:49 +0300

    add implementations

---
+++ GetandTakeAPI/Program.cs
@@ -0,0 +1,88 @@
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using AutoMapper;
+using GetandTake.Business.DependencyResolvers.Autofac;
+using GetandTake.Business.Services.AutoMapper;
+using GetandTake.Configuration.Extensions;
+using GetandTake.Configuration.Settings;
+using GetandTake.Core.DependencyResolvers;
+using GetandTake.Core.Extensions;
+using GetandTake.Core.Utilities.IoC;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerUI;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddDependencyResolvers(new ICoreModule[]{
+    new CoreModule()
+});
+
+builder.Services.AddSingleton(new MapperConfiguration(mapperConfig =>
+                                                      mapperConfig.AddProfile(new AutoMapperProfile())).CreateMapper());
+builder.Services.AddResponseCaching(options =>
+{
+    options.MaximumBodySize = 1024;
+    options.UseCaseSensitivePaths = true;
+});
+
+var databaseSettings = builder.Configuration.GetSection(nameof(AppSettings.Database)).Get<DatabaseSettings>();
+var productSettings = builder.Configuration.GetSection(nameof(AppSettings.Products)).Get<ProductsSettings>();
+var hostSettings = builder.Configuration.GetSection(nameof(AppSettings.Host)).Get<HostSettings>();
+var logFilterSettings = builder.Configuration.GetSection(nameof(AppSettings.LoggingParameters)).Get<LogFilterSettings>();
+
+var appSettings = new AppSettings
+{
+    Database = databaseSettings,
+    Products = productSettings,
+    Host = hostSettings,
+    LoggingParameters = logFilterSettings
+};
+
+builder.Services.RegisterServices(appSettings);
+builder.Services.RegisterDatabase(appSettings);
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(swaggerGen =>
+{
+    swaggerGen.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "GetandTake.API",
+        Version = "v1",
+        Description = "NET 6",
+        Contact = new OpenApiContact
+        {
+            Name = "GetandTake API Project"
+        }
+    });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(swaggerUI =>
+    {
+        swaggerUI.SwaggerEndpoint("/swagger/v1/swagger.json", "Api Service");
+        swaggerUI.DefaultModelsExpandDepth(-1);
+        swaggerUI.DocExpansion(DocExpansion.None);
+    });
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthentication();
+
+app.UseAuthorization();
+
+app.ExceptionHandler();
+
+app.UseResponseCaching();
+
+app.MapControllers();
+
+app.Run();
