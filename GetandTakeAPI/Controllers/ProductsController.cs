4d3f2a01b70b7c7f3eb6ad40900986a3934033e6
CerenSusuz <ceren_susuz@epam.com>
2022-11-24 11:20:54 +0300

    created controllers

---
+++ GetandTakeAPI/Controllers/ProductsController.cs
@@ -0,0 +1,31 @@
+﻿using GetandTake.Business.Services.Abstract;
+using GetandTake.Models.DTOs.ListDTO;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GetandTakeAPI.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ProductsController : ControllerBase
+{
+    private readonly IProductService _productService;
+
+    public ProductsController(IProductService productService)
+    {
+        _productService = productService;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<List<ProductsDTO>>> GetAllProducts()
+    {
+        var products = await _productService.GetAllAsync();
+
+        return Ok(products);
+    }
+
+    [HttpGet("id/{id:int}")]
+    public async Task<ProductsDTO> GetProductByIdAsync(int id)
+    {
+        return await _productService.GetByIdAsync(id);
+    }
+}
