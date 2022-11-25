ce09f6cfabf1bece7545c1043a671bbfef93ce1c
CerenSusuz <ceren_susuz@epam.com>
2022-11-25 11:08:07 +0300

    fixed db errors

--- GetandTake/Migrations/20221124224240_adddbc.Designer.cs
+++ GetandTake/Migrations/20221124224240_adddbc.Designer.cs
@@ -4,6 +4,7 @@ using GetandTake.DataAccess;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace GetandTake.Migrations
 {
     [DbContext(typeof(NorthwindDbContext))]
-    partial class NorthwindDbContextModelSnapshot : ModelSnapshot
+    [Migration("20221124224240_adddbc")]
+    partial class adddbc
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
