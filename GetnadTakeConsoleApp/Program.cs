7d3f83ce7c9cff7782c65fd5987ae3b6b59f870f
CerenSusuz <ceren_susuz@epam.com>
2022-11-25 13:23:19 +0300

    checked console client and editted

--- GetnadTakeConsoleApp/Program.cs
+++ GetnadTakeConsoleApp/Program.cs
@@ -1,35 +1,46 @@
-﻿using System.Net.Http.Headers;
+﻿using GetandTake.Models.DTOs.ResponseDTO;
+using System.Net.Http.Headers;
 
-static void Main()
+HttpClient client = new()
 {
-    RunAsync().GetAwaiter().GetResult();
-}
+    BaseAddress = new Uri("https://localhost:7167/api")
+};
+client.DefaultRequestHeaders.Accept.Clear();
+client.DefaultRequestHeaders.Accept.Add(
+    new MediaTypeWithQualityHeaderValue("application/json"));
 
-static async Task RunAsync()
+try
 {
+    List<ProductResponse> products = null;
+    List<CategoryResponse> categories = null;
+    var productsResponse = await client.GetAsync($"{client.BaseAddress}/products");
+    var categoriesResponse = await client.GetAsync($"{client.BaseAddress}/categories");
 
-    HttpClient client = new()
+    if (productsResponse.IsSuccessStatusCode)
     {
-        BaseAddress = new Uri("https://localhost:7167/api")
-    };
-    client.DefaultRequestHeaders.Accept.Clear();
-    client.DefaultRequestHeaders.Accept.Add(
-        new MediaTypeWithQualityHeaderValue("application/json"));
-
-    try
-    {
-        var products = await client.GetAsync($"{client.BaseAddress}/products");
-        var categories = await client.GetAsync($"{client.BaseAddress}/categories");
-
-        if (products.IsSuccessStatusCode && categories.IsSuccessStatusCode)
+        products = await productsResponse.Content.ReadAsAsync<List<ProductResponse>>();
+        Console.WriteLine("Products");
+        foreach (var product in products)
         {
-            Console.WriteLine($"Products : {products} / Categories {categories}");
-        }      
-    }
-    catch (Exception exception)
-    {
-        Console.WriteLine(exception.Message);
+            Console.WriteLine($"ProductName:{product.ProductName}\n");
+        }
     }
 
-    Console.ReadLine();
+
+    if (categoriesResponse.IsSuccessStatusCode)
+    {
+        categories = await categoriesResponse.Content.ReadAsAsync<List<CategoryResponse>>();
+        Console.WriteLine("Categories");
+        foreach (var category in categories)
+        {
+            Console.WriteLine($"CategoryName:{category.CategoryName}\n");
+        }
+    }
+
 }
+catch (Exception exception)
+{
+    Console.WriteLine(exception.Message);
+}
+
+Console.ReadLine();
