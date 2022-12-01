85dbb448c50c424829de32763a9cb103a82f01b7
CerenSusuz <ceren_susuz@epam.com>
2022-12-01 14:27:42 +0300

    IdentityUserConfiguration

---
+++ GetandTake/DataAccess/Configuration/IdentityUserConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+
+namespace GetandTake.DataAccess.Configuration;
+
+public class IdentityUserConfiguration : IEntityTypeConfiguration<IdentityUser>
+{
+    public void Configure(EntityTypeBuilder<IdentityUser> builder)
+    {
+        builder.HasKey(primaryId => primaryId.Id);
+        builder.Property(property => property.UserName).HasMaxLength(256);
+        builder.HasMany<IdentityUserClaim<string>>()
+            .WithOne()
+            .HasForeignKey(foreignKey => foreignKey.UserId)
+            .IsRequired();
+    }
+}
