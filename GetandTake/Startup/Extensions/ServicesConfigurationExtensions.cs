393f25c9492682c78b0f51e15f0d30b677f29204
CerenSusuz <ceren_susuz@epam.com>
2022-12-01 14:26:38 +0300

    addIdentity

--- GetandTake/Startup/Extensions/ServicesConfigurationExtensions.cs
+++ GetandTake/Startup/Extensions/ServicesConfigurationExtensions.cs
@@ -3,7 +3,9 @@ using GetandTake.Configuration.Settings;
 using GetandTake.Core.DependencyResolvers;
 using GetandTake.Core.Extensions;
 using GetandTake.Core.Utilities.IoC;
+using GetandTake.DataAccess;
 using GetandTake.Startup.Configuration;
+using Microsoft.AspNetCore.Identity;
 
 namespace GetandTake.Startup.Extensions;
 
@@ -11,6 +13,29 @@ public static class ServicesConfigurationExtensions
 {
     public static void Configure(this IServiceCollection services, AppSettings appSettings)
     {
+        services.AddIdentity<IdentityUser, IdentityRole>(options =>
+            {
+                options.SignIn.RequireConfirmedAccount = true;
+                options.SignIn.RequireConfirmedAccount = true;
+
+                options.Password.RequireDigit = true;
+                options.Password.RequireLowercase = true;
+                options.Password.RequireNonAlphanumeric = true;
+                options.Password.RequireUppercase = true;
+                options.Password.RequiredLength = 6;
+                options.Password.RequiredUniqueChars = 1;
+
+                options.User.RequireUniqueEmail = true;
+                options.User.AllowedUserNameCharacters =
+                "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
+
+                options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+                options.Lockout.MaxFailedAccessAttempts = 5;
+                options.Lockout.AllowedForNewUsers = true;
+            })
+            .AddEntityFrameworkStores<NorthwindDbContext>()
+            .AddDefaultTokenProviders();
+
         services.AddDependencyResolvers(new ICoreModule[]
         {
             new CoreModule()
@@ -33,6 +58,8 @@ public static class ServicesConfigurationExtensions
         services.RegisterServices(appSettings);
 
         services.RegisterDatabase(appSettings);
+
+        services.RegisterGithub(appSettings);
 
         services.AddEndpointsApiExplorer();
 
