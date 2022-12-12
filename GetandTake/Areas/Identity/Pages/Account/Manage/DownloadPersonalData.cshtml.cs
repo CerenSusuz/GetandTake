943cf594378d490833b80ceec0e60602852140ac
CerenSusuz <ceren_susuz@epam.com>
2022-12-13 00:00:04 +0300

    .

---
+++ GetandTake/Areas/Identity/Pages/Account/Manage/DownloadPersonalData.cshtml.cs
@@ -0,0 +1,59 @@
+﻿using System.Text.Json;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace GetandTake.Areas.Identity.Pages.Account.Manage;
+
+public class DownloadPersonalDataModel : PageModel
+{
+    private readonly UserManager<IdentityUser> _userManager;
+    private readonly ILogger<DownloadPersonalDataModel> _logger;
+
+    public DownloadPersonalDataModel(
+        UserManager<IdentityUser> userManager,
+        ILogger<DownloadPersonalDataModel> logger)
+    {
+        _userManager = userManager;
+        _logger = logger;
+    }
+
+    public IActionResult OnGet()
+    {
+        return NotFound();
+    }
+
+    public async Task<IActionResult> OnPostAsync()
+    {
+        var user = await _userManager.GetUserAsync(User);
+
+        if (user == null)
+        {
+            return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
+        }
+
+        _logger.LogInformation("User with ID '{UserId}' asked for their personal data.", _userManager.GetUserId(User));
+
+        var personalData = new Dictionary<string, string>();
+        var personalDataProps = typeof(IdentityUser).GetProperties().Where(
+                        prop => Attribute.IsDefined(prop, typeof(PersonalDataAttribute)));
+
+        foreach (var property in personalDataProps)
+        {
+            personalData.Add(property.Name, property.GetValue(user)?.ToString() ?? "null");
+        }
+
+        var logins = await _userManager.GetLoginsAsync(user);
+
+        foreach (var loginProp in logins)
+        {
+            personalData.Add($"{loginProp.LoginProvider} external login provider key", loginProp.ProviderKey);
+        }
+
+        personalData.Add($"Authenticator Key", await _userManager.GetAuthenticatorKeyAsync(user));
+
+        Response.Headers.Add("Content-Disposition", "attachment; filename=PersonalData.json");
+
+        return new FileContentResult(JsonSerializer.SerializeToUtf8Bytes(personalData), "application/json");
+    }
+}
