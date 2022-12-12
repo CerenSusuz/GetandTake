663985fb826c78645f2ab7ef2bb24e14a709728e
CerenSusuz <ceren_susuz@epam.com>
2022-12-12 22:59:52 +0300

    changed identity table names

--- GetandTake/DataAccess/NorthwindDbContext.cs
+++ GetandTake/DataAccess/NorthwindDbContext.cs
@@ -2,10 +2,11 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity;
+using GetandTake.Core.Models.Account;
 
 namespace GetandTake.DataAccess;
 
-public class NorthwindDbContext : IdentityDbContext<IdentityUser>
+public class NorthwindDbContext : IdentityDbContext<ApplicationUser>
 {
     public NorthwindDbContext(DbContextOptions<NorthwindDbContext> options) : base(options)
     {
@@ -14,6 +15,36 @@ public class NorthwindDbContext : IdentityDbContext<IdentityUser>
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
+
+        modelBuilder.HasDefaultSchema("Identity");
+        modelBuilder.Entity<IdentityUser>(entity =>
+        {
+            entity.ToTable(name: "User");
+        });
+        modelBuilder.Entity<IdentityRole>(entity =>
+        {
+            entity.ToTable(name: "Role");
+        });
+        modelBuilder.Entity<IdentityUserRole<string>>(entity =>
+        {
+            entity.ToTable("UserRoles");
+        });
+        modelBuilder.Entity<IdentityUserClaim<string>>(entity =>
+        {
+            entity.ToTable("UserClaims");
+        });
+        modelBuilder.Entity<IdentityUserLogin<string>>(entity =>
+        {
+            entity.ToTable("UserLogins");
+        });
+        modelBuilder.Entity<IdentityRoleClaim<string>>(entity =>
+        {
+            entity.ToTable("RoleClaims");
+        });
+        modelBuilder.Entity<IdentityUserToken<string>>(entity =>
+        {
+            entity.ToTable("UserTokens");
+        });
 
         modelBuilder.ApplyConfigurationsFromAssembly(typeof(NorthwindDbContext).Assembly);        
     }
