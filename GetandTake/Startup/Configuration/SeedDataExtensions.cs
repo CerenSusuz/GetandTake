5818f7942b4d5e64a5ad1bac4e6e8fd49c108123
CerenSusuz <ceren_susuz@epam.com>
2022-12-14 12:03:05 +0300

    add seeddataextensions for roles

---
+++ GetandTake/Startup/Configuration/SeedDataExtensions.cs
@@ -0,0 +1,30 @@
+﻿using GetandTake.DataAccess.Seed;
+using GetandTake.DataAccess;
+using Microsoft.AspNetCore.Identity;
+
+namespace GetandTake.Startup.Configuration;
+
+public static class SeedDataExtensions
+{
+    public static async void ConfigureSeedData(this WebApplication app)
+    {
+        using (var scope = app.Services.CreateScope())
+        {
+            var services = scope.ServiceProvider;
+            var loggerFactory = services.GetRequiredService<ILoggerFactory>();
+            try
+            {
+                var context = services.GetRequiredService<NorthwindDbContext>();
+                var userManager = services.GetRequiredService<UserManager<IdentityUser>>();
+                var roleManager = services.GetRequiredService<RoleManager<IdentityRole>>();
+                await ContextSeed.SeedRolesAsync(roleManager);
+                await ContextSeed.SeedAdminAsync(userManager, roleManager);
+            }
+            catch (Exception ex)
+            {
+                var logger = loggerFactory.CreateLogger<Program>();
+                logger.LogError(ex, "An error occurred seeding the DB.");
+            }
+        }
+    }
+}
