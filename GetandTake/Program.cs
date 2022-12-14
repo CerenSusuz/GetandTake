fd96ecbe369236e7a7bf23236cbb6e9a188e12b0
CerenSusuz <ceren_susuz@epam.com>
2022-12-14 12:03:25 +0300

    clean code

--- GetandTake/Program.cs
+++ GetandTake/Program.cs
@@ -1,10 +1,6 @@
 using GetandTake.Configuration.Extensions;
-using GetandTake.DataAccess;
-using GetandTake.DataAccess.Seed;
 using GetandTake.Startup.Configuration;
 using GetandTake.Startup.Extensions;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.Extensions.Hosting;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.RegisterLogging();
@@ -16,25 +12,6 @@ builder.Services.Configure(appSettings);
 builder.Services.RegisterAzure(builder.Configuration, appSettings);
 
 var app = builder.Build();
-
-using (var scope = app.Services.CreateScope())
-{
-    var services = scope.ServiceProvider;
-    var loggerFactory = services.GetRequiredService<ILoggerFactory>();
-    try
-    {
-        var context = services.GetRequiredService<NorthwindDbContext>();
-        var userManager = services.GetRequiredService<UserManager<IdentityUser>>();
-        var roleManager = services.GetRequiredService<RoleManager<IdentityRole>>();
-        await ContextSeed.SeedRolesAsync(roleManager);
-        await ContextSeed.SeedAdminAsync(userManager, roleManager);
-    }
-    catch (Exception ex)
-    {
-        var logger = loggerFactory.CreateLogger<Program>();
-        logger.LogError(ex, "An error occurred seeding the DB.");
-    }
-}
 
 app.Configure();
 
